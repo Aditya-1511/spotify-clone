dc678d9596f9242500400e6995580417c9a90152
Aditya Pandey <aditya1511ge@gmail.com>
2024-02-21 23:06:16 +0530

    Added two modules to left sidebar bottom component

--- src/components/left_sidebar-bottom/LeftSidebarBottom.jsx
+++ src/components/left_sidebar-bottom/LeftSidebarBottom.jsx
@@ -16,6 +16,16 @@ function LeftSidebarBottom() {
             <FaPlus />
           </div>
         </div>
+        <div className="create_your_playlist">
+          <h4>Create your first playlist</h4>
+          <h6>It's easy, we'll help you</h6>
+          <button className="create_playlist_button">Create playlist</button>
+        </div>
+        <div className="lets_find_some_podcasts">
+          <h4>Let's find some podcasts to follow</h4>
+          <h6>We'll keep you updated on new episodes</h6>
+          <button className="browse_podcasts_button">Browse podcasts</button>
+        </div>
       </div>
     </>
   );
