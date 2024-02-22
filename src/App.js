a76f56ea5d1315d501ad418c50d77dff29b39f7d
Aditya-1511 <aditya1511ge@gmail.com>
2024-02-22 11:18:06 +0530

    Added routing and fixed button hovering bugs

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import LeftSidebarBottom from "./components/left_sidebar-bottom/LeftSidebarBotto
 import LeftSidebarTop from "./components/left_sidebar-top/LeftSidebarTop";
 import RightSidebarBottom from "./components/right_sidebar-bottom/RightSidebarBottom";
 import RightSidebarTop from "./components/right_sidebar-top/RightSidebarTop";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="mainWrapper">
-      <div className="main">
-        <div className="lefSide">
-          <LeftSidebarTop />
-          <LeftSidebarBottom />
+    <BrowserRouter>
+      <div className="mainWrapper">
+        <div className="main">
+          <div className="lefSide">
+            <Routes><Route path="/" element={<LeftSidebarTop />} /></Routes>
+            <Routes><Route path="/" element={<LeftSidebarBottom />} /></Routes>
+          </div>
+          <div className="rightSide">
+            <Routes><Route path="/" element={<RightSidebarTop />} /></Routes>
+            <Routes><Route path="/" element={<RightSidebarBottom />} /></Routes>
+          </div>
         </div>
-        <div className="rightSide">
-          <RightSidebarTop />
-          <RightSidebarBottom />
+        <div className="footer_main">
+          <Routes><Route path="/" element={<Footer />} /></Routes>
         </div>
       </div>
-      <div className="footer_main">
-        <Footer />
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./footer.css";
+import { useNavigate } from "react-router-dom";
 
 function Footer() {
+  const navigate = useNavigate();
+
+  const handleFooterClick = () => {
+    navigate('/signup')
+  }
   return (
     <>
-      <div className="footer">
+      {/* <a href="https://www.spotify.com/in-en/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2F"> */}
+      <div className="footer" onClick={handleFooterClick}>
         <div className="text">
           <h5 className="footer_h5">Preview of Spotify</h5>
           <h6 className="footer_h6">
@@ -13,9 +20,10 @@ function Footer() {
           </h6>
         </div>
         <div className="sign_up_for_free">
-          <button className="sign_up_for_free_button">Sign up for free</button>
+          <button className="sign_up_for_free_button" href="https://www.spotify.com/in-en/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2F">Sign up for free</button>
         </div>
       </div>
+      {/* </a> */}
     </>
   );
 }
